b5dc9eb0e19fd92bb2444f439ea9e1a72d7787e2
duran.hsieh <duran.hsieh@afusion.com>
2017-04-05 15:03:22 +0800

    update

--- app/src/main/java/com/example/duranhsieh/gpsexample/MainActivity.java
+++ app/src/main/java/com/example/duranhsieh/gpsexample/MainActivity.java
@@ -1,25 +1,35 @@
 package com.example.duranhsieh.gpsexample;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private TextView textStatus;
-    private Button buttonGPS;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textStatus = (TextView) findViewById(R.id.gps_display);
-        buttonGPS = (Button) findViewById(R.id.gps_click);
+        textStatus = (TextView) findViewById(R.id.message_display);
+        button = (Button) findViewById(R.id.button_click);
 
-        buttonGPS.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
                 textStatus.setText("clicked!");
